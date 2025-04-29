49fd72db9665ed9122d9a1ac6f6f73658064f8d6
DevOrbite300K <bdjouma37@gmail.com>
2025-04-29 12:23:42 +0000

    Ajout des fichiers

---
+++ src/app/app.routes.ts
@@ -0,0 +1,20 @@
+import { Routes } from '@angular/router';
+import path from 'node:path';
+import { AccueilComponent } from './composants/accueil/accueil.component';
+import { ConnexionComponent } from './composants/connexion/connexion.component';
+import { InscriptionComponent } from './composants/inscription/inscription.component';
+
+export const routes: Routes = [
+
+    
+    {
+        path:"accueil", component:AccueilComponent, pathMatch: 'full' 
+    },
+    //{ path: '', redirectTo: '/accueil', pathMatch: 'full' },
+    {
+        path:"connexion", component:ConnexionComponent
+    },
+    {
+        path:"inscription", component:InscriptionComponent
+    }
+];
---
+++ src/app/composants/connexion/connexion.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterModule, RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-connexion',
+  standalone: true,
+  imports: [RouterOutlet, RouterModule],
+  templateUrl: './connexion.component.html',
+  styleUrl: './connexion.component.scss'
+})
+export class ConnexionComponent {
+    
+}
